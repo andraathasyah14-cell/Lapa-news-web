c2f426148768de64dc06f62c82fae90188838ea2
andra athasyah <andraathasyah11@gmail.com>
2025-09-16 08:11:49 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/updates/submission-form.tsx
+++ src/components/updates/submission-form.tsx
@@ -146,7 +146,7 @@ export default function UpdateSubmissionForm({ countries }: { countries: Country
                 <Input type="number" {...field} />
               </FormControl>
               <FormMessage />
-            </FormMItem>
+            </FormItem>
           )}
         />
          <FormField
